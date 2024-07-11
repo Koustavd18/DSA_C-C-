877adbbfcd48371c54a1b212329f0c618e811a10
Koustav Das <kdkdas8@gmail.com>
2024-07-11 22:36:40 +0530

    structs

--- array.cpp
+++ array.cpp
@@ -4,16 +4,21 @@ using namespace std;
 int main() {
 	int n;
 	cout<<"enter the size of the array"<<endl;
-	cin>>n;
+	if(n<=0){
+		cout<<"Size should be greater than zero"<<endl;
+	}else{
+		cin>>n;
+	}
 	int arr[n];
-	for(int i=0; i<=n; i++){
+	for(int i=0; i<n; i++){
 		int elem;
 		cout<<"Enter element"<<endl;
 		cin>>elem;
 		arr[i] = elem;
 	}
-	for(int i=0;i<n;++i){
-		cout<<arr[i]<<endl;
+	cout<<"\n"<<endl;
+	for(int i:arr){
+		cout<<i<<endl;
 	}
 	return 0;
 }
